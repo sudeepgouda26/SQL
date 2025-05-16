701f7546deb44c64311c14c3cb405ed2ec4cc1ce
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-05-16 20:59:04 +0530

    basic'

--- WindowFunction/rankAndDenseRank.sql
+++ WindowFunction/rankAndDenseRank.sql
@@ -32,14 +32,64 @@ What are the performance considerations of using ranking functions in large data
 
 🔹 Query-Based Scenario Questions
 Write a query to fetch the top 3 highest salaries using RANK().
+ 1  select * from (select emp.*,  rank() over(order by sal desc ) as rank from emp)
+  2* where rank <=3
+SQL> /
+
+     EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO           RANK
+---------- ---------- --------- ---------- --------- ---------- ---------- ---------- ----------
+      7839 KING       PRESIDENT            17-NOV-81       5000               10           1
+      7788 SCOTT      ANALYST         7566 19-APR-87       3000               20           2
+      7902 FORD       ANALYST         7566 03-DEC-81       3000               20           2
 
 Write a query to fetch the top 3 highest salaries using DENSE_RANK().
+
+  1  select * from(select emp.*, dense_rank() over(order by sal) as rank from emp)
+  2* where rank in (1,2,3)
+SQL> /
+
+     EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO           RANK
+---------- ---------- --------- ---------- --------- ---------- ---------- ---------- ----------
+      7900 JAMES      CLERK           7698 03-DEC-81        950               30           1
+      7369 SMITH      SECURITY        7902 17-DEC-80        990               20           2
+      7876 ADAMS      CLERK           7788 23-MAY-87       1100               20           3
+
 
 Show how RANK() and DENSE_RANK() produce different results on duplicate salaries.
 
 Write a query to get the 5th highest salary using DENSE_RANK().
 
+  1  select * from(select emp.*, dense_rank() over(order by sal desc) as rank from emp)
+  2* where rank =5
+SQL> /
+
+     EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO           RANK
+---------- ---------- --------- ---------- --------- ---------- ---------- ---------- ----------
+      7782 CLARK      MANAGER         7839 09-JUN-81       2450               10           5
+
+
 How would you rank employees by salary within each department?
+
+  1  select *
+  2* from (select emp.*, dense_rank() over(partition by deptno order by sal desc) as rank from emp)
+SQL> /
+
+     EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO           RANK
+---------- ---------- --------- ---------- --------- ---------- ---------- ---------- ----------
+      7839 KING       PRESIDENT            17-NOV-81       5000               10           1
+      7782 CLARK      MANAGER         7839 09-JUN-81       2450               10           2
+      7934 MILLER     CLERK           7782 23-JAN-82       1300               10           3
+      7788 SCOTT      ANALYST         7566 19-APR-87       3000               20           1
+      7902 FORD       ANALYST         7566 03-DEC-81       3000               20           1
+      7566 JONES      MANAGER         7839 02-APR-81       2975               20           2
+      7876 ADAMS      CLERK           7788 23-MAY-87       1100               20           3
+      7369 SMITH      SECURITY        7902 17-DEC-80        990               20           4
+      7698 BLAKE      MANAGER         7839 01-MAY-81       2850               30           1
+      7499 ALLEN      SECURITY        7698 20-FEB-81       1600        300    30           2
+      7844 TURNER     SALESMAN        7698 08-SEP-81       1500          0    30           3
+      7654 MARTIN     SALESMAN        7698 28-SEP-81       1250       1400    30           4
+      7521 WARD       SALESMAN        7698 22-FEB-81       1250        500    30           4
+      7900 JAMES      CLERK           7698 03-DEC-81        950               30           5
 
 How can you find employees who share the same rank within their department?
 
@@ -49,4 +99,4 @@ Demonstrate the difference between ROW_NUMBER(), RANK(), and DENSE_RANK() with a
 
 Write a query to fetch employees with the second highest salary using RANK().
 
-How would you filter only the top N rows per group using DENSE_RANK()?
+/
